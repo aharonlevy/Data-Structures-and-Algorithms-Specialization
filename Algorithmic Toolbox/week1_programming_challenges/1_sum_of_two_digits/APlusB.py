74a1ce71b68237710a996134276869c5b7a26e78
Aharon Levy <mr.aharon.levy@gmail.com>
2023-05-14 14:29:17 +0300

    Made the code more 'pythonic'

--- Algorithmic Toolbox/week1_programming_challenges/1_sum_of_two_digits/APlusB.py
+++ Algorithmic Toolbox/week1_programming_challenges/1_sum_of_two_digits/APlusB.py
@@ -4,9 +4,7 @@ numbers that the user will write in
 '''
 
 def sum_of_two_digits(first_digit, second_digit):
-    '''
-    Function will return the sum two given numbers
-    '''
+    """Return the sum of two given numbers."""
     return first_digit + second_digit
 
 
--- Algorithmic Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ Algorithmic Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,25 +1,20 @@
 def max_pairwise_product(numbers):
+    '''Return the max product of the numbers from the numbers list'''
     n = len(numbers)
     max_product = 0
     biggest_num = -1
     second_biggest = -1
-    if (n < 2):
-        return max_product
-    elif (numbers[0] >= numbers[1]):
-        biggest_num = numbers[0]
-        second_biggest = numbers [1]
-    else:
-        biggest_num = numbers[1]
-        second_biggest = numbers [0]
-    for index in range(2, n):
-        if (numbers[index] >= biggest_num):
-            second_biggest = biggest_num
-            biggest_num = numbers[index]
-        elif (numbers[index] > second_biggest):
-            second_biggest = numbers[index]
+    assert n >= 2, "List must have at least 2 elements"
+
+    biggest_num = max(numbers[0], numbers[1])
+    second_biggest = min(numbers[0], numbers[1])
+    for _ in range(2, n):
+        biggest_num = max(numbers)
+        numbers.remove(biggest_num)
+        second_biggest = max(numbers)
+
     max_product = biggest_num * second_biggest
     return max_product
-
 
 if __name__ == '__main__':
     _ = int(input())
