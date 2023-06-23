9d1270a49945f5a298702bb2e7057aeb26a6b452
Aharon Levy <mr.aharon.levy@gmail.com>
2023-06-23 17:28:27 +0300

    week 1 solved Q2

--- Data Structures/week1_basic_data_structures/2_tree_height/tree_height.py
+++ Data Structures/week1_basic_data_structures/2_tree_height/tree_height.py
@@ -18,75 +18,80 @@ def compute_height(n, parents):
 
 
 def compute_height_fast(n, parents):
-    root = -1
     if len(parents) <= 1:
         return len(parents)
     hash_array = [[] for _ in range(len(parents))]
+    root = -1
     for index, parent in enumerate(parents):
         if parent == -1:
             root = index
-            continue
-        hash_array[parent].append(index)
-    return get_max_height(hash_array, root, 1)
+        else:
+            hash_array[parent].append(index)
+    memo = [0] * len(parents)
+    return get_max_height(hash_array, root, memo)
 
 
-def get_max_height(hash_array, root_index, cur_len):
-    max_hight = 0
-    if len(hash_array[root_index]) == 0:
-        return cur_len
+def get_max_height(hash_array, root_index, memo):
+    if not hash_array[root_index]:
+        return 1
+    if memo[root_index] != 0:
+        return memo[root_index]
+    max_height = 0
     for child in hash_array[root_index]:
-        max_hight = max(max_hight, get_max_height(hash_array, child, (cur_len + 1)))
-    return max_hight
+        height = 1 + get_max_height(hash_array, child, memo)
+        max_height = max(max_height, height)
+    memo[root_index] = max_height
+    return max_height
 
 
 def main():
-    # n = int(input())
-    # parents = list(map(int, input().split()))
-    # print(compute_height(n, parents))
+    n = int(input())
+    parents = list(map(int, input().split()))
+    print(compute_height_fast(n, parents))
 
     # manual tests
 
     # empty case
 
-    n = 0
-    parents = []
-    if compute_height_fast(n, parents=parents) != 0:
-        print("# empty case failed")
-    else:
-        print("# empty case passed")
+    # n = 0
+    # parents = []
+    # if compute_height_fast(n, parents=parents) != 0:
+    #     print("# empty case failed")
+    # else:
+    #     print("# empty case passed")
 
-    # hight 1
-    n = 1
-    parents = [-1]
-    if compute_height_fast(n, parents=parents) != 1:
-        print("# hight 1 test failed")
-    else:
-        print("# hight 1 test passed")
-    # hight 2
-    n = 5
-    parents = [3, 3, 3, -1, 3]
-    if compute_height_fast(n, parents=parents) != 2:
-        print("# hight 2 test failed")
-        print("# got: ", compute_height_fast(n, parents=parents), " when expected 2")
-    else:
-        print("# hight 2 test passed")
-        # hight 2 simple
-        n = 2
-    parents = [-1, 0]
-    if compute_height_fast(n, parents=parents) != 2:
-        print("# hight 2 test failed")
-        print("# got: ", compute_height_fast(n, parents=parents), " when expected 2")
-    else:
-        print("# hight 2 simple test passed")
+    # # hight 1
+    # n = 1
+    # parents = [-1]
+    # if compute_height_fast(n, parents=parents) != 1:
+    #     print("# hight 1 test failed")
+    # else:
+    #     print("# hight 1 test passed")
+    # # hight 2
+    # n = 5
+    # parents = [3, 3, 3, -1, 3]
+    # if compute_height_fast(n, parents=parents) != 2:
+    #     print("# hight 2 test failed")
+    #     print("# got: ", compute_height_fast(n, parents=parents), " when expected 2")
+    # else:
+    #     print("# hight 2 test passed")
+    #     # hight 2 simple
+    #     n = 2
+    # parents = [-1, 0]
+    # if compute_height_fast(n, parents=parents) != 2:
+    #     print("# hight 2 test failed")
+    #     print("# got: ", compute_height_fast(n, parents=parents), " when expected 2")
+    # else:
+    #     print("# hight 2 simple test passed")
 
-        # case: hight 4
-        n = 5
-    parents = [1, 2, 3, -1, 3]
-    if compute_height_fast(n, parents=parents) != 4:
-        print("# hight 4 test failed")
-        print("# got: ", compute_height_fast(n, parents=parents), " when expected 4")
-    else:
-        print("# hight 4 test passed")
+    #     # case: hight 4
+    #     n = 5
+    # parents = [1, 2, 3, -1, 3]
+    # if compute_height_fast(n, parents=parents) != 4:
+    #     print("# hight 4 test failed")
+    #     print("# got: ", compute_height_fast(n, parents=parents), " when expected 4")
+    # else:
+    #     print("# hight 4 test passed")
 
 
 # In Python, the default limit on recursion depth is rather low,
