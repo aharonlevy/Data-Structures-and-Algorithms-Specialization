c5187e3ea648c90860be631449460a943094bba1
Aharon Levy <mr.aharon.levy@gmail.com>
2023-07-17 09:31:45 +0300

    solved week 2 Q1 added week 2 declarations

--- Data Structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ Data Structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -32,33 +32,53 @@ def build_heap_fast(data):
             min_child = left_child
         else:
             min_child = min(left_child, right_child, key=lambda idx: data[idx])
-        if data[index] > data[min_child]:
-            # this part is incorrect need to create a siftdown function
-            swaps.append([index, min_child])
-            data[min_child], data[index] = data[index], data[min_child]
+        cur_add = sift_down(data, index, min_child)
+        for element in cur_add:
+            swaps.append(element)
         index -= 1
     return swaps
 
 
+def sift_down(data, index, min_child):
+    swaps_to_add = []
+    while index < len(data):
+        if data[index] > data[min_child]:
+            swaps_to_add.append([index, min_child])
+            data[index], data[min_child] = data[min_child], data[index]
+            index = min_child
+        else:
+            break
+        left_child = (index * 2) + 1
+        right_child = left_child + 1
+        if right_child >= len(data):
+            if left_child >= len(data):
+                break
+            min_child = left_child
+        else:
+            min_child = min(left_child, right_child, key=lambda idx: data[idx])
+
+    return swaps_to_add
+
+
 def main():
-    # n = int(input())
-    # data = list(map(int, input().split()))
-    # assert len(data) == n
-
-    # swaps = build_heap_fast(data)
-
-    # print(len(swaps))
-    # for i, j in swaps:
-    #     print(i, j)
-
-    # test
-    data = [5, 4, 3, 2, 1]
+    n = int(input())
+    data = list(map(int, input().split()))
+    assert len(data) == n
 
     swaps = build_heap_fast(data)
 
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
+
+    # test
+    # data = [5, 4, 3, 2, 1]
+
+    # swaps = build_heap_fast(data)
+
+    # print(len(swaps))
+    # for i, j in swaps:
+    #     print(i, j)
 
 
 if __name__ == "__main__":
